164a0a2785cd75c0502e2e608d82f3e3aa6aca97
hweiyu <hwy_dod@163.com>
2018-05-11 17:42:51 +0800

    push resource code

---
+++ src/main/java/com/hwy/algorithm/Mytest.java
@@ -0,0 +1,15 @@
+package com.hwy.algorithm;
+
+/**
+ * @author huangweiyu
+ * @version V1.0
+ * @Title: 描述
+ * @Description: 描述
+ * @date 2018/5/11 17:33
+ **/
+public class Mytest {
+
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
